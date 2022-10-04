3b0472d008e4f5441c9d6f0e91ce71ba233db650
klimenth <klimenth@bitbucket.securegroup.zone>
2022-10-04 08:06:55 +0300

    New

--- src/test/java/dressesMenu/DressesMenuTests.java
+++ src/test/java/dressesMenu/DressesMenuTests.java
@@ -62,6 +62,56 @@ public class DressesMenuTests extends BaseTest {
         String actualAddTextTwo = alertPage.getTextFromElement(alertPage.headerTwo_SuccessAddProduct);
         Assertions.assertEquals("There is 1 item in your cart.", actualAddTextTwo);
 
+        // Go to Shopping cart page and check table with added product
+        alertPage.proceed_btn.click();
+        shopCartPage.waitToBeVisible(shopCartPage.tableWith_product,10);
+        shopCartPage.scrollToElement(shopCartPage.tableWith_product);
+    }
+    @Test
+    @Order(2)
+    @DisplayName("Add product to shopping cart and proceed to checkout")
+    void verifyCheckoutInShoppingCart_TC2(){
+        homePage = new HomePage(driver);
+        dressPage = new DressesPage(driver);
+        shopCartPage = new ShoppingCartPage(driver);
+        alertPage = new AlertPage(driver);
+        authPage = new AuthenticationPage(driver);
+        accPage = new AccountPage(driver);
+
+        //Sing in with valid credentials
+        homePage.signIn_btn.click();
+        authPage.setRegisteredMail("mislead@mail.bg");
+        authPage.setRegisteredPass("Kiko123@");
+        authPage.clickSignInButton();
+        accPage.waitToBeVisible(accPage.welcomeMessage,10);
+        String actualMessageAfterCreateAccount = "Welcome to your account. Here you can manage all of your personal information and orders.";
+        Assertions.assertEquals(actualMessageAfterCreateAccount,accPage.getTextFromElement(accPage.welcomeMessage));
+
+        // Go to Dress menu
+        homePage.menuDresses.click();
+        dressPage.scrollToElement(dressPage.subcategoriesMenu_Header);
+        //Mark cotton filter, and one product left on the page
+        dressPage.cottonFilter.click();
+        dressPage.waitToBeVisible(dressPage.showingResults, 10);
+        dressPage.waitToBeVisible(dressPage.enabledFilters, 30);
+        dressPage.getListElements(dressPage.enabledFiltersList);
+        System.out.println("Filters are set: "+ dressPage.enabledFiltersList.getText());
+        dressPage.waitRefreshedResultText(dressPage.showingResults, 40,"Showing 1 - 1 of 1 items");
+        Assertions.assertEquals("Showing 1 - 1 of 1 items",dressPage.showingResults.getText());
+        // Add to shopping cart
+        dressPage.list_btn.click();
+        dressPage.waitToBeVisible(dressPage.addCart_btn_PrintedDress,10);
+        dressPage.addCart_btn_PrintedDress.click();
+
+        // Wait to be visible alert page for adding product
+        alertPage.waitToBeVisible(alertPage.addingProduct_layer, 10);
+        driver.switchTo().activeElement();
+        // Check the product is added and wait proceed checkout
+        String actualAddTextOne = alertPage.getTextFromElement(alertPage.headerOne_SuccessAddProduct);
+        Assertions.assertEquals("Product successfully added to your shopping cart", actualAddTextOne);
+        String actualAddTextTwo = alertPage.getTextFromElement(alertPage.headerTwo_SuccessAddProduct);
+        Assertions.assertEquals("There is 1 item in your cart.", actualAddTextTwo);
+
         // Go to Shopping cart page
         alertPage.proceed_btn.click();
         shopCartPage.waitToBeVisible(shopCartPage.tableWith_product,10);
@@ -98,5 +148,7 @@ public class DressesMenuTests extends BaseTest {
         shopCartPage.scrollToElement(shopCartPage.ordersTable_header);
         Assertions.assertEquals("ORDER HISTORY",shopCartPage.ordersTable_header.getText());
     }
+
+
 
 }
